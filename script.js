a846143ac0a3f6af039de7c4e9ec5a64098716f6
Tomás Aguilar <soytomasaguilar@gmail.com>
2024-11-05 12:28:52 -0300

    Mobile first home

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Traducir() {
     if (textoEs.style.display === 'none') {
         textoEs.style.display = 'block';
         textoEn.style.display = 'none';
-        boton.innerText = 'Traducir a Inglés';
+        boton.innerText = 'Translate to English';
     } else {
         textoEs.style.display = 'none';
         textoEn.style.display = 'block';
@@ -14,3 +14,16 @@ function Traducir() {
     }
 }
 
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#menu");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+    nav.classList.remove("hidden");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    nav.classList.add("hidden");
+})
